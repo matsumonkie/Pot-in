590cadc682dee458302a4a9975dfe9d99b2a0562
Iori Matsuhara <matsuhar@gmail.com>
2014-01-18 22:29:35 +0100

    remove pseudo from attributes

---
+++ db/migrate/20140118212803_drop_pseudo_from_user.rb
@@ -0,0 +1,5 @@
+class DropPseudoFromUser < ActiveRecord::Migration
+  def change
+    remove_column :users, :pseudo
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140118195718) do
+ActiveRecord::Schema.define(version: 20140118212803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20140118195718) do
   create_table "users", force: true do |t|
     t.string   "firstname"
     t.string   "lastname"
-    t.string   "pseudo"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email",                  default: "", null: false
