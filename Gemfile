01fb93e084c1486d205dd46422dbb96495edda2e
Iori Matsuhara <matsuhar@gmail.com>
2014-01-18 17:54:25 +0100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'bootstrap-sass',         '~> 3.0'
+gem 'coffee-rails',           '~> 4.0.0'
+gem 'draper',                 '~> 1.3'
+gem 'jbuilder',               '~> 1.2'
+gem 'jquery-rails'
+gem 'pg'
+gem 'rails',                  '~> 4.0'
+gem 'sass-rails',             '~> 4.0.0'
+gem 'slim-rails',             '~> 2.0'
+gem 'turbolinks'
+gem 'uglifier',               '>= 1.3.0'
+
+group :development do
+  gem 'better_errors',        '~> 1.1'
+  gem 'quiet_assets',         '~> 1.0'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Valet::Application.config.secret_key_base = '9c45b468fc2f640478004ce40d4d2046e78d5646945cf8c4b57b1a7ce6fa3a3443fed5bfb4382840fdc1e4ce19b96b4c8a2834362354cd6e2ee94547cfaea9d9'
