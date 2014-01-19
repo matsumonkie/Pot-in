7fb074dc2464e6d21376bb41f2f6a190f84d6a17
Iori Matsuhara <matsuhar@gmail.com>
2014-01-19 23:07:17 +0100

    ++

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
-
   private
   
   def after_sign_out_path_for(resource)
@@ -12,4 +10,5 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     root_path
   end
+
 end
---
+++ app/controllers/signed_in_controller.rb
@@ -0,0 +1,4 @@
+class SignedInController < ApplicationController
+  before_action :authenticate_user!
+  
+end
