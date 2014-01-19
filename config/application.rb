936b68025277ad4a4d3b05f0e8e9f99477101507
Iori Matsuhara <matsuhar@gmail.com>
2014-01-19 22:56:04 +0100

    ++

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,7 @@ module Valet
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    I18n.enforce_available_locales = false
     config.i18n.default_locale = :fr
     config.i18n.available_locales = %i[fr]
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Valet::Application.routes.draw do
 
-  resources :homes
-
   devise_for :users
 
   root 'home#index'
 
-  resources :users
+  #resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
