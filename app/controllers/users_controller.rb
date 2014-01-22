65635ec36c371716512834f839f9982a18a321a2
Iori Matsuhara <matsuhar@gmail.com>
2014-01-22 22:34:52 +0100

    restrict user permission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,35 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
   def index
-    @users = User.all
+    redirect_to user_path(id: current_user.id) unless current_user.id == 1
   end
+
+  def show    
+    redirect_to root_path unless current_user.id == @user.id    
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+      else
+        format.html { render action: 'edit' }
+      end
+    end
+  end    
+  
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:firstname, :lastname)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Valet::Application.routes.draw do
 
   root 'home#index'
 
-  #resources :users
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
