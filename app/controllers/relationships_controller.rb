c57ed85723807c20bbf5cd775e5a450d2f982b6d
Iori Matsuhara <matsuhar@gmail.com>
2014-01-25 09:58:52 +0100

    grouping relationships by type done in controller instead of view

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,6 +1,11 @@
 class RelationshipsController < SignedInController
   nested_belongs_to :user
 
+  def index
+    raw_relationships = Relationship.where(user_id: current_user)    
+    @relationships = raw_relationships.group_by(&:relation_type)    
+  end
+
   private
   
   def permitted_params
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,5 @@
 class Relationship < ActiveRecord::Base
   belongs_to :user
   belongs_to :contact, class_name: "User"
+ 
 end
