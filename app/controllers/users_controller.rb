0805c5e4641853936265fa8ff37596de1f73f173
Iori Matsuhara <matsuhar@gmail.com>
2014-01-29 23:09:04 +0100

    edit user is working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < SignedInController
-  actions :except => [:create, :index]
+#  actions :except => [:create, :index]
+
+  def update    
+    flash[:notice] = t('event.success-edit-user', user_name: current_user.decorate.fullname)
+    update! { user_path }
+  end
 
   private
 
