931022de5a51d4cbdc52a112efb8a4c06429d081
Iori Matsuhara <matsuhar@gmail.com>
2014-02-02 22:00:37 +0100

    refactor user model, now have debtors/creditors, purchases/debts as well

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,6 @@
 class Payment < ActiveRecord::Base
   belongs_to :user
   belongs_to :contact, class_name: "User"
+
+  #scope :all_payments, where("user_id = ? OR contact_id = ?", user_id, user_id)
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,13 @@ class User < ActiveRecord::Base
   has_many :relationships
   has_many :contacts, :through => :relationships
 
-  has_many :payments
-  has_many :contact, :through => :payments
+  has_many :creditors, through: :purchases, source: :contact
+  has_many :debtors, through: :debts, source: :user
+
+  has_many :debts, class_name: 'Payment', foreign_key: :contact_id
+  has_many :purchases, class_name: 'Payment'
+
+  def payments
+    debts + purchases
+  end
 end
