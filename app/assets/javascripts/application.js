340e6b0ed1b4a1b7373f9026ade19250fa1d162d
Iori Matsuhara <matsuhar@gmail.com>
2014-02-04 23:29:05 +0100

    application call global javascript files as well as controller script

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,4 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap-switch
-//= require_tree .
+//= require_directory ./global/
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
   def render_md(text)
     @@markdown.render(text).html_safe
   end
+
+  def script_exists?(script)    
+    script = "#{Rails.root}/app/assets/javascripts/#{params[:controller]}.js"
+    available_extensions = %w[.coffee .erb .coffee.erb] + [""]
+    available_extensions.inject(false) do |truth, extension|
+      truth || File.exists?("#{script}#{extension}")
+    end
+  end
 end
