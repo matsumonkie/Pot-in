8d9f45794bb8ec4032b0ef11edb2e42ed44a64c6
Iori Matsuhara <matsuhar@gmail.com>
2014-02-15 21:34:13 +0100

    ++

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,12 @@
 class Account < ActiveRecord::Base
   belongs_to :user, class_name: "User"
   belongs_to :contact, class_name: "User"
+
+  def positive?
+    balance > 0
+  end
+
+  def negative?
+    !positive?
+  end
 end
